c17bef4316e58e4b1fcf1b7aac582457551de92d
[al-ninsima] <[al.ninsima@gmail.com]>
2025-11-05 21:34:18 +0000

    Enhancement 1: Advanced Trade Search System...RsqlSpecificationBuilder...vscode flagging issue with Rsqlvisitor but think bc i haven't done that one yest

--- backend/src/main/java/com/technicalchallenge/specification/RsqlSpecificationBuilder.java
+++ backend/src/main/java/com/technicalchallenge/specification/RsqlSpecificationBuilder.java
@@ -1,4 +1,14 @@
 package com.technicalchallenge.specification;
 
+import com.technicalchallenge.model.Trade;
+import cz.jirutka.rsql.parser.RSQLParser;
+import cz.jirutka.rsql.parser.ast.Node;
+import org.springframework.data.jpa.domain.Specification;
+
 public class RsqlSpecificationBuilder {
+
+    public static Specification<Trade> build(String query) {
+        Node rootNode = new RSQLParser().parse(query);
+        return rootNode.accept(new RsqlVisitor<>());
+    }
 }
