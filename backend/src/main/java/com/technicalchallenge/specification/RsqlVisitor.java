f448f1d4ac15e479fd432eff702c895c76b111f2
[al-ninsima] <[al.ninsima@gmail.com]>
2025-11-05 22:09:24 +0000

    Enhancement 1: Advanced Trade Search System...RsqlVisitor written tick tick tick

--- backend/src/main/java/com/technicalchallenge/specification/RsqlVisitor.java
+++ backend/src/main/java/com/technicalchallenge/specification/RsqlVisitor.java
@@ -1,5 +1,28 @@
 package com.technicalchallenge.specification;
 
-public class RsqlVisitor {
+import org.springframework.data.jpa.domain.Specification;
+import cz.jirutka.rsql.parser.ast.*;
 
+public class RsqlVisitor<T> implements RSQLVisitor<Specification<T>, Void> {
+
+    @Override
+    public Specification<T> visit(AndNode node, Void param) {
+        return node.getChildren().stream()
+                .map(child -> child.accept(this, null))
+                .reduce(Specification::and)
+                .orElse(null);
+    }
+
+    @Override
+    public Specification<T> visit(OrNode node, Void param) {
+        return node.getChildren().stream()
+                .map(child -> child.accept(this, null))
+                .reduce(Specification::or)
+                .orElse(null);
+    }
+
+    @Override
+    public Specification<T> visit(ComparisonNode node, Void param) {
+        return new RsqlSpecification<>(node);
+    }
 }
