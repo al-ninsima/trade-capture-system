ea6c94060691834a03134f99b215ec97e1dd2882
[al-ninsima] <[al.ninsima@gmail.com]>
2025-11-06 20:51:03 +0000

    added a specification file - RsqlSearchOperation...rsql wasn't working because my previous method was not jakarta compatible so would not build / run.

---
+++ backend/src/main/java/com/technicalchallenge/specification/RsqlSearchOperation.java
@@ -0,0 +1,7 @@
+package com.technicalchallenge.specification;
+
+public enum RsqlSearchOperation {
+    EQUAL, NOT_EQUAL, GREATER_THAN, GREATER_THAN_OR_EQUAL,
+    LESS_THAN, LESS_THAN_OR_EQUAL, IN, NOT_IN
+}
+
